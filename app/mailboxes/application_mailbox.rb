34d44147004c35eddb8bf02a1cce02ee520264cd
Vincent Ollivier <v@vinc.cc>
2024-08-27 23:51:36 +0200

    Add mailbox

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,3 +1,3 @@
 class ApplicationMailbox < ActionMailbox::Base
-  # routing /something/i => :somewhere
+  routing(/^gpt@/i => :gpt)
 end
---
+++ app/mailboxes/gpt_mailbox.rb
@@ -0,0 +1,4 @@
+class GptMailbox < ApplicationMailbox
+  def process
+  end
+end
