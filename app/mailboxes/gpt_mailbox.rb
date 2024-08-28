324cc5c0cdfe7a25f20b69a2252e72fcc41257b9
Vincent Ollivier <v@vinc.cc>
2024-08-28 19:49:42 +0200

    Add more logs

--- app/mailboxes/gpt_mailbox.rb
+++ app/mailboxes/gpt_mailbox.rb
@@ -1,10 +1,14 @@
 class GptMailbox < ApplicationMailbox
   def process
     Rails.logger.info { "Processing email from '#{mail.from}' to '#{mail.to}':" }
+    Rails.logger.info { "-------------------------------" }
     Rails.logger.info { mail }
+    Rails.logger.info { "-------------------------------" }
+    Rails.logger.info { mail.body.decoded }
+    Rails.logger.info { "-------------------------------" }
     if User.where(email: mail.from, status: ["paid", "admin"]).exists?
       question = mail.body.decoded
-      response = chat(question)
+      #response = chat(question)
       #BotMailer.reply(mail.to, mail.from, mail.subject, response).deliver_now
     else
       #bounce_with BotMailer.bounce(mail.from, mail.subject).deliver_now
