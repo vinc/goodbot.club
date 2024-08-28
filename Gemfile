7bbd7098e3567bc16ab8744bd928c338f07ea2f5
Vincent Ollivier <v@vinc.cc>
2024-08-28 09:58:09 +0200

    Add goodjob gem

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem "devise"
 gem "devise-i18n"
 
 gem "devise-bootstrap5"
+
+gem "good_job", "~> 4.2"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "good_bot_club_production"
+  config.active_job.queue_adapter = :good_job
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
