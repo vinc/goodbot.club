2c90a5b6529b6054e5283da216641c26d31beeaf
Vincent Ollivier <v@vinc.cc>
2024-08-28 11:55:34 +0200

    Add GoodJob dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  authenticate :user, ->(user) { user.admin? } do
+    mount GoodJob::Engine => "jobs"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
