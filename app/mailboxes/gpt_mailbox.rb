47b6c8a7440238f882cb5924d57e1639f4e34680
Vincent Ollivier <v@vinc.cc>
2024-08-28 09:16:01 +0200

    Add paid users

--- app/mailboxes/gpt_mailbox.rb
+++ app/mailboxes/gpt_mailbox.rb
@@ -1,6 +1,6 @@
 class GptMailbox < ApplicationMailbox
   def process
-    if User.where(email: mail.from).exists?
+    if User.where(email: mail.from, status: ["paid", "admin"]).exists?
       question = mail.body.decoded
       response = chat(question)
       BotMailer.reply(mail.to, mail.from, mail.subject, response).deliver_now
