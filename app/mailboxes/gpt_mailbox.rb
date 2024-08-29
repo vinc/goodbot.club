afcb3ef665807ec17589e1f3f9ff6fa9e6f66bcf
Vincent Ollivier <v@vinc.cc>
2024-08-29 11:50:45 +0200

    Enable GPT

--- app/mailboxes/gpt_mailbox.rb
+++ app/mailboxes/gpt_mailbox.rb
@@ -1,20 +1,11 @@
 class GptMailbox < ApplicationMailbox
   def process
-    Rails.logger.info { "Processing email from '#{mail.from}' to '#{mail.to}':" }
-    Rails.logger.info { "-------------------------------" }
     Rails.logger.info { mail }
-    Rails.logger.info { "-------------------------------" }
     if User.where(email: mail.from.first, status: ["paid", "admin"]).exists?
       req = (mail.text_part || mail.body).decoded
-      Rails.logger.info { "-------------------------------" }
-      Rails.logger.info { req }
-      Rails.logger.info { "-------------------------------" }
-      # res = chat(req)
-      res = "Got your email!"
-      Rails.logger.info { "Replying" }
+      res = chat(req)
       BotMailer.reply(mail, req, res).deliver_now
     else
-      Rails.logger.info { "Bouncing" }
       # bounce_with BotMailer.bounce(mail).deliver_now
       bounced!
     end
@@ -26,7 +17,7 @@ class GptMailbox < ApplicationMailbox
     client = OpenAI::Client.new
     res = client.chat(
       parameters: {
-          model: "gpt-4o",
+          model: "gpt-4o-mini",
           messages: [{ role: "user", content: req }],
           temperature: 0.7
       }
