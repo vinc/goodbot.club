4005e1a42342fc677d0e389385bf810e9aece002
Vincent Ollivier <v@vinc.cc>
2024-08-29 17:59:50 +0200

    Add markdown options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      fenced_code_blocks: true,
+      autolink: true
+    })
     markdown.render(text).html_safe
   end
 
   def wrap(text, prefix: "")
-    #WordWrap.ww(text, 72).lines.map { |line| "#{prefix}#{line}" }.join
+    # text = WordWrap.ww(text, 72)
     text.lines.map { |line| "#{prefix}#{line}" }.join
   end
 end
