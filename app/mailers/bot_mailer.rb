751a3f12189a68ca274b3c1454b5b382e6d5e09b
Vincent Ollivier <v@vinc.cc>
2024-08-29 10:37:06 +0200

    Use in_reply_to

--- app/mailers/bot_mailer.rb
+++ app/mailers/bot_mailer.rb
@@ -1,10 +1,10 @@
 class BotMailer < ApplicationMailer
   def bounce(message_id, user, subject)
-    mail(reply_to: message_id, to: user, subject: subject)
+    mail(in_reply_to: message_id, to: user, subject: subject)
   end
 
   def reply(message_id, bot, user, subject, response)
     @response = response
-    mail(reply_to: message_id, from: bot, to: user, subject: subject)
+    mail(in_reply_to: message_id, from: bot, to: user, subject: subject)
   end
 end
